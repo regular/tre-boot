4afe1db664b22c4e3356d202e2d4b3c108fe0225
Jan Bölsche <regular.gonzales@googlemail.com>
2019-01-15 10:53:30 +0100

    Implement redirect to blob

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const url = require('url')
 const fs = require('fs')
-const Trumpet = require('trumpet')
-const Browserify = require('browserify')
+const WatchMerged = require('tre-prototypes')
+const {isMsg} = require('ssb-ref')
+
+function awaitStable(ssb, bootKey, cb) {
+  const watchMerged = WatchMerged(ssb)
+
+  const bootObs = watchMerged(boot)
+  let timer, release
+  release = watch(bootObs, kv => {
+    if (!kv) return
+    console.log(kv)
+    if (timer) clearTimeout(timer)
+    timer = setTimeout( ()=> {
+      const blob = kv.value.content.codeBlob
+      const url = blob && `/blobs/get/${encodeURIComponent(blob)}`
+      release()
+      cb(url ? null : new Error('malformed boot message: ' + kv.key), url)
+    }, 400)
+  })
+}
 
 exports.name = 'tre-boot'
 exports.version = require('./package.json').version
@@ -13,21 +31,16 @@ exports.init = function (ssb, config) {
   ssb.ws.use(function (req, res, next) {
     if(!(req.method === "GET" || req.method == 'HEAD')) return next()
     const u = url.parse('http://makeurlparseright.com'+req.url)
-    if (u.pathname == '/boot') {
-      res.setHeader('Content-Type', 'text/html')
-
-      const trumpet = Trumpet()
-      trumpet.pipe(res)
-       
-      const bootMsg = trumpet.select('.boot-msg').createWriteStream()
-      bootMsg.end(`${config.boot}`)
-
-      const bootCode = trumpet.select('.boot-code').createWriteStream()
-      const browserify = Browserify()
-      browserify.add(__dirname + '/client.js')
-      browserify.bundle().pipe(bootCode)
-
-      fs.createReadStream(__dirname + '/boot.html').pipe(trumpet)
+    if (u.pathname.startsWith('/boot')) {
+      const bootKey = decodeURIComponent(u.pathname.slice(6)) || config.boot
+      if (!isMsg(bootKey)) {
+        return res.end('Bad Request: Invalid boot message id syntax: ' + bootKey, 400)
+      }
+      awaitStable(ssb, bootKey, (err, url) => {
+        if (err) return res.end(err.message, 503)
+        res.setHeader('Location', url)
+        res.end('Current revision at ' + url, 307)
+      })
       return
     }
     next()
